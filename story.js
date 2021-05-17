33e10a66e4e0df3f8094c86b857f8147c293e3c2
iyasalowaneh <iyas_moh@yahoo.com>
2021-05-17 15:49:18 +0300

    this is new, i love it

---
+++ story.js
@@ -0,0 +1,4 @@
+let name = "iyas";
+let age = 26;
+let hoppy = "coding";
+console.log (`Mr. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hoppy} practice!`)
